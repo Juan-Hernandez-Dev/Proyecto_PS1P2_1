d71cbdc78fc2eff16e654ff0458064e6c12e9f44
Aída <aida19962006@gmail.com>
2025-06-21 14:19:16 -0600

    simplificacion de shuffle

--- src/codiguito.cpp
+++ src/codiguito.cpp
@@ -2,9 +2,11 @@
 #include <string>
 #include <cstdlib>
 #include <ctime>
+#include <algorithm>  // para shuffle
+#include <random>     // para random_device y mt19937
 using namespace std;
 
-const int TOTAL_CARTAS = 56;
+const int TOTAL_CARTAS = 52;
 
 union ValorCarta {
     int numero;
@@ -27,25 +29,14 @@ string nombreCarta(int valor) {
 }
 
 void generarCartas(Carta mazo[]) {
-    int indice = 0;
     string palos[] = {"Picas", "Treboles", "Corazones", "Diamantes"};
-
-    for (int m = 0; m < 4; m++) {
-        for (int i = 0; i < 4; i++) {
-            for (int valor = 1; valor <= 13; valor++) {
-                if (indice < TOTAL_CARTAS) {
-                    mazo[indice].valor.numero = valor;
-                    mazo[indice].palo = palos[i];
-                    indice++;
-                }
-            }
+    int indice = 0;
+    for (int i = 0; i < 4; i++) {
+        for (int valor = 1; valor <= 13; valor++) {
+            mazo[indice].valor.numero = valor;
+            mazo[indice].palo = palos[i];
+            indice++;
         }
-    }
-
-    while (indice < TOTAL_CARTAS) {
-        mazo[indice].valor.numero = 1;
-        mazo[indice].palo = palos[indice % 4];
-        indice++;
     }
 }
 
@@ -55,38 +46,18 @@ void mostrarCartas(Carta mazo[]) {
         cout << nombreCarta(mazo[i].valor.numero) << " de " << mazo[i].palo << endl;
     }
 }
-void mergeShuffleMitadesAleatorio(Carta mazo[]) {
-    Carta copia[TOTAL_CARTAS];
-    int mitad = TOTAL_CARTAS / 2;
-    int izquierda = 0;
-    int derecha = mitad;
-    int k = 0;
 
-    while (izquierda < mitad && derecha < TOTAL_CARTAS) {
-        if (rand() % 2 == 0) {
-            copia[k++] = mazo[izquierda++];
-        } else {
-            copia[k++] = mazo[derecha++];
-        }
-    }
-
-    while (izquierda < mitad) copia[k++] = mazo[izquierda++];
-    while (derecha < TOTAL_CARTAS) copia[k++] = mazo[derecha++];
-
-    for (int i = 0; i < TOTAL_CARTAS; i++) {
-        mazo[i] = copia[i];
-    }
+void mezclarCartas(Carta mazo[]) {
+    random_device rd;
+    mt19937 g(rd());
+    shuffle(mazo, mazo + TOTAL_CARTAS, g);
 }
-void mezclarVariasVeces(Carta mazo[], int repeticiones) {
-    for (int i = 0; i < repeticiones; i++) {
-        mergeShuffleMitadesAleatorio(mazo);
-    }
-}
+
 int main() {
     srand(time(0));
     Carta mazo[TOTAL_CARTAS];
     generarCartas(mazo);
-    mezclarVariasVeces(mazo, 3);
+    mezclarCartas(mazo);
     mostrarCartas(mazo);
     return 0;
 }
